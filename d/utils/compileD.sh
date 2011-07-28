97b37bed6e9d67ff8288a7ad7d5cf01ddfd950fd
GreatEmerald <pastas4@gmail.com>
2011-07-28 23:00:09 +0300

    A few changes to Linux compilation of libarcomage. Still won't link there.

--- d/utils/compileD.sh
+++ d/utils/compileD.sh
@@ -2,7 +2,7 @@
 # Script for rebuilding the D library on Linux.
 # GreatEmerald, 2011
 cd ../src
-dmd -m64 -c -lib ArcomageD.d
+dmd -m64 -c -lib -fPIC ArcomageD.d
 cp ArcomageD.a ../../lib/libArcomageD.a
 rm ArcomageD.a
 echo "Finished rebuilding the D library."
