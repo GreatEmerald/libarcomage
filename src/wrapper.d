98236c7e41ea856ef5be375be73e8449f61a2dff
GreatEmerald <pastas4@gmail.com>
2011-10-03 22:29:14 +0300

    Initialisation works fine so far, as does the initial SetPlayerInfo(). I have to use rather hacky methods to pull in configuration data, though.
    Signed-off-by: GreatEmerald <pastas4@gmail.com>

--- src/wrapper.d
+++ src/wrapper.d
@@ -18,6 +18,31 @@ void SetPlayerInfo(int PlayerNum, char* Name, bool AI)
 {
     Player[PlayerNum].AI = AI;
     Player[PlayerNum].Name = to!string(Name);
-    foreach (Stats P; Player)
-        writeln("Player No. ", PlayerNum, " is known as ", P.Name, " and is an AI ", P.AI);
+    foreach (int i, Stats P; Player)
+        writeln("Player No. ", i, " is known as ", P.Name, " and is an AI ", P.AI);
+}
+
+int GetConfig(int Type)
+{
+    switch (Type)
+    {
+        case 0: return cast(int)Config.Fullscreen;
+        case 1: return cast(int)Config.SoundEnabled;
+        case 2: return cast(int)Config.CardTranslucency;
+        case 3: return cast(int)Config.CardsInHand;
+        case 4: return Config.TowerLevels;
+        case 5: return Config.WallLevels;
+        case 6: return Config.QuarryLevels;
+        case 7: return Config.MagicLevels;
+        case 8: return Config.DungeonLevels;
+        case 9: return Config.BrickQuantities;
+        case 10: return Config.GemQuantities;
+        case 11: return Config.RecruitQuantities;
+        case 12: return Config.MaxWall;
+        case 13: return Config.ResourceVictory;
+        case 14: return cast(int)Config.OneResourceVictory;
+        case 15: return cast(int)Config.UseOriginalCards;
+        case 16: return cast(int)Config.UseOriginalMenu;
+        default: return 0;
+    }
 }
