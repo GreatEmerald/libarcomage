9efe3f704d8d033b11a781f6674ad5d9ce4352d9
GreatEmerald <pastas4@gmail.com>
2014-11-07 22:27:29 +0200

    Make compilation into a shared library
    No point in having a static library compiled, after all.
    
    Signed-off-by: GreatEmerald <pastas4@gmail.com>

--- utils/compile.sh
+++ utils/compile.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 cd ../src
 #rdmd --chatty --build-only -w -wi -lib -L-llua -I../include/LuaD arco.d cards.d
-dmd -m64 -c -lib -od../lib -oflibarcomage -version=clibrary -L-llua -I../include/LuaD *.d ../include/LuaD/luad/*.d ../include/LuaD/luad/conversions/*.d ../include/LuaD/luad/c/*.d
+dmd -m64 -c -fPIC -oflibarcomage.o -L-llua -I../include/LuaD *.d ../include/LuaD/luad/*.d ../include/LuaD/luad/conversions/*.d ../include/LuaD/luad/c/*.d
+dmd -m64 -shared -defaultlib=libphobos2.so -oflibarcomage.so libarcomage.o
+rm libarcomage.o
+mv libarcomage.so ../lib
 cd ../utils
